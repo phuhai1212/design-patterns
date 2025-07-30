48c4fd2d42700f7b89dc379e8edf735becd8e3de
phuhai1212 <vphoa34@gmail.com>
2025-07-30 14:30:33 +0700

    update

--- src/main/java/com/example/designpatterns/algorithm/hard/SuperEggDrop.java
+++ src/main/java/com/example/designpatterns/algorithm/hard/SuperEggDrop.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SuperEggDrop {
-
+    private static Map<String, Integer> memo = new HashMap<>();
     public static int superEggDrop(int k, int n) {
         int m = 0;
         while (solve(k, m) < n){
@@ -17,7 +17,13 @@ public class SuperEggDrop {
         if (m == 0 || k == 0){
             return 0;
         }
-        return solve(k - 1, m - 1) + solve(k, m - 1) + 1;
+        String key = k + "_" + m;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int result = solve(k - 1, m - 1) + solve(k, m - 1) + 1;
+        memo.put(key, result);
+        return result;
     }
 
 }
